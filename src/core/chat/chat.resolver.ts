41175d94208e5ca9955a20ee9bdc7dce0baa193b
feral <feral.x@yandex.ru>
2025-06-08 16:15:09 +1100

    "chunks chat, id chat protect"

--- src/core/chat/chat.resolver.ts
+++ src/core/chat/chat.resolver.ts
@@ -1,46 +1,43 @@
 import {Args, Mutation, Resolver, Subscription, Query} from '@nestjs/graphql';
 import { ChatService } from './chat.service';
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import * as process from "node:process";
 import {MessageModel} from "./dto/message.model";
+import {RedisPubSub} from "graphql-redis-subscriptions";
+import {DeepseekMessageModel} from "./dto/deepseek.message.model";
 
 
 const pubSub = new RedisPubSub({
-  connection : {
+  connection: {
     host: process.env.HOST,
-    port: Number(process.env.REDIS_PORT)
-  }
-})
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
 @Resolver(() => MessageModel)
 export class ChatResolver {
   constructor(private readonly chatService: ChatService) {}
   
   
-  @Query(() => Boolean)
-  getMessage(){
-    return true
+  @Query(()=> String)
+  ping(){
+    return "pong"
   }
   
   @Mutation(() => Boolean)
-  async sendMessage(
-      @Args("user") user: string,
-      @Args("sender")  sender: string,
-      @Args("message") message: string,
+  async startChat(
+      @Args('userId') userId: string,
+      @Args('messages', { type: () => [DeepseekMessageModel] }) messages: DeepseekMessageModel[],
+      @Args('message') message: string,
   ) {
-    await pubSub.publish(`chat:${user}`, {
-      chat: {
-        sender, message
-      }
-    });
-    return true
+    await this.chatService.chatStream(messages, message, userId);
+    return true;
   }
-  
   
   @Subscription(() => MessageModel, {
     name: 'chat',
-    resolve: (payload) => payload.chat
+    resolve: (payload) => payload.chat,
   })
-  chatTrigger(@Args("user") user: string) {
-    return pubSub.asyncIterator(`chat:${user}`);
+  chat(@Args('userId') userId: string) {
+    return pubSub.asyncIterator(`chat:${userId}`);
   }
+  
 }
---
+++ src/core/chat/dto/deepseek.message.model.ts
@@ -0,0 +1,9 @@
+import {Field, InputType, ObjectType} from "@nestjs/graphql";
+
+@InputType()
+export class DeepseekMessageModel {
+	@Field(() => String)
+	role: string;
+	@Field(() => String)
+	content: string;
+}
